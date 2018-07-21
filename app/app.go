a8530f6f46b6deb6afdc76f9ed6fa45394244021
Steve Gravrock <sdg@panix.com>
2018-07-21 13:54:24 -0700

    Don't pass dependencies around app by hand

--- app/app.go
+++ app/app.go
@@ -23,130 +23,144 @@ func Run(baseUrl string, savecreds bool, authenticator auth.Authenticator,
 	downloader dl.Downloader, fileStore storage.Storage, clock clock.Clock,
 	stdout io.Writer, stderr io.Writer) int {
 
-	err := fileStore.EnsureRoot()
+	ftg := application{
+		baseUrl:       baseUrl,
+		savecreds:     savecreds,
+		authenticator: authenticator,
+		downloader:    downloader,
+		fileStore:     fileStore,
+		clock:         clock,
+		stdout:        stdout,
+		stderr:        stderr,
+	}
+	return ftg.Run()
+}
+
+type application struct {
+	baseUrl       string
+	savecreds     bool
+	authenticator auth.Authenticator
+	downloader    dl.Downloader
+	fileStore     storage.Storage
+	clock         clock.Clock
+	stdout        io.Writer
+	stderr        io.Writer
+}
+
+func (ftg *application) Run() int {
+	err := ftg.fileStore.EnsureRoot()
 	if err != nil {
 		msg := err.Error()[5:] // strip leading "stat "
-		fmt.Fprintln(stderr, msg)
+		fmt.Fprintln(ftg.stderr, msg)
 		return 1
 	}
 
-	httpClient, err := authenticator.Authenticate(savecreds)
+	httpClient, err := ftg.authenticator.Authenticate(ftg.savecreds)
 	if err != nil {
-		fmt.Fprintln(stderr, err.Error())
+		fmt.Fprintln(ftg.stderr, err.Error())
 		return 1
 	}
-	flickrClient := flickrapi.NewClient(httpClient, baseUrl)
+	flickrClient := flickrapi.NewClient(httpClient, ftg.baseUrl)
 
-	photos, err := downloader.DownloadPhotolist(flickrClient, fileStore)
+	photos, err := ftg.downloader.DownloadPhotolist(flickrClient, ftg.fileStore)
 	if err != nil {
-		fmt.Fprintf(stderr, "Error downloading photo list: %s\n", err.Error())
+		fmt.Fprintf(ftg.stderr, "Error downloading photo list: %s\n",
+			err.Error())
 		return 1
 	}
 
-	updatedPhotoIds, err := getUpdatedPhotoIds(flickrClient, downloader, fileStore, stderr)
+	updatedPhotoIds, err := ftg.getUpdatedPhotoIds(flickrClient)
 	if err != nil {
-		fmt.Fprintf(stderr, "Error downloading the list of updated photos: %s\n", err.Error())
+		fmt.Fprintf(ftg.stderr, "Error downloading the list of updated photos: %s\n", err.Error())
 	}
 
-	fmt.Fprintf(stdout, "Got %d photos\n", len(photos))
+	fmt.Fprintf(ftg.stdout, "Got %d photos\n", len(photos))
 
 	for _, p := range photos {
 		id, err := p.Id()
 		if err != nil {
-			fmt.Fprintln(stderr, err.Error())
+			fmt.Fprintln(ftg.stderr, err.Error())
 			return 1
 		}
 
-		if shouldDownloadInfo(downloader, fileStore, id, updatedPhotoIds) {
-			err = downloader.DownloadPhotoInfo(flickrClient, fileStore, id)
+		if ftg.shouldDownloadInfo(id, updatedPhotoIds) {
+			err = ftg.downloader.DownloadPhotoInfo(flickrClient,
+				ftg.fileStore, id)
 			if err != nil {
-				fmt.Fprintf(stderr, "Error downloading info for %s: %s\n",
+				fmt.Fprintf(ftg.stderr,
+					"Error downloading info for %s: %s\n",
 					id, err.Error())
 				return 1
 			}
 		}
 
-		if shouldDownloadOriginal(downloader, fileStore, id, updatedPhotoIds) {
-			err = downloader.DownloadOriginal(httpClient, fileStore, p)
+		if ftg.shouldDownloadOriginal(id, updatedPhotoIds) {
+			err = ftg.downloader.DownloadOriginal(httpClient, ftg.fileStore, p)
 			if err != nil {
-				fmt.Fprintf(stderr, "Error downloading original for %s: %s\n",
+				fmt.Fprintf(ftg.stderr,
+					"Error downloading original for %s: %s\n",
 					id, err.Error())
 				return 1
 			}
 		}
 	}
 
-	err = moveDeletedFiles(fileStore, stderr, photos, "photo-info",
-		"info")
+	err = ftg.moveDeletedFiles(photos, "photo-info", "info")
 	if err != nil {
-		fmt.Fprintln(stderr, err.Error())
+		fmt.Fprintln(ftg.stderr, err.Error())
 		return 1
 	}
 
-	err = moveDeletedFiles(fileStore, stderr, photos, "originals",
-		"original")
+	err = ftg.moveDeletedFiles(photos, "originals", "original")
 	if err != nil {
-		fmt.Fprintln(stderr, err.Error())
+		fmt.Fprintln(ftg.stderr, err.Error())
 		return 1
 	}
 
-	err = timestamp.Write(clock, fileStore)
+	err = timestamp.Write(ftg.clock, ftg.fileStore)
 	if err != nil {
-		fmt.Fprintf(stderr, "Error saving timestamp: %s\n", err.Error())
+		fmt.Fprintf(ftg.stderr, "Error saving timestamp: %s\n", err.Error())
 		return 1
 	}
 
 	return 0
 }
 
-func shouldDownloadOriginal(downloader dl.Downloader,
-	fileStore storage.Storage,
+func (ftg *application) shouldDownloadOriginal(
 	photoId string,
 	updatedPhotoIds []string) bool {
 
 	return updatedPhotoIds == nil ||
 		containsString(updatedPhotoIds, photoId) ||
-		!downloader.OriginalExists(fileStore, photoId)
+		!ftg.downloader.OriginalExists(ftg.fileStore, photoId)
 }
 
-func shouldDownloadInfo(downloader dl.Downloader,
-	fileStore storage.Storage,
+func (ftg *application) shouldDownloadInfo(
 	photoId string,
 	updatedPhotoIds []string) bool {
 
 	return updatedPhotoIds == nil ||
 		containsString(updatedPhotoIds, photoId) ||
-		!downloader.PhotoInfoExists(fileStore, photoId)
+		!ftg.downloader.PhotoInfoExists(ftg.fileStore, photoId)
 }
 
-func containsString(haystack []string, needle string) bool {
-	for _, candidate := range haystack {
-		if candidate == needle {
-			return true
-		}
-	}
+func (ftg *application) getUpdatedPhotoIds(
+	flickr flickrapi.Client) ([]string, error) {
 
-	return false
-}
-
-func getUpdatedPhotoIds(flickr flickrapi.Client, downloader dl.Downloader,
-	fileStore storage.Storage, stderr io.Writer) ([]string, error) {
-
-	lastRun := timestamp.Read(fileStore, stderr)
+	lastRun := timestamp.Read(ftg.fileStore, ftg.stderr)
 	if lastRun == 0 {
 		return nil, nil
 	}
 
-	return downloader.GetRecentPhotoIds(lastRun, flickr)
+	return ftg.downloader.GetRecentPhotoIds(lastRun, flickr)
 }
 
-func moveDeletedFiles(fileStore storage.Storage,
-	stderr io.Writer,
+func (ftg *application) moveDeletedFiles(
 	photos []flickrapi.PhotoListEntry,
 	srcDir string,
 	description string) error {
 
-	srcFiles, err := fileStore.ListFiles(srcDir)
+	srcFiles, err := ftg.fileStore.ListFiles(srcDir)
 	if err != nil {
 		if os.IsNotExist(err) {
 			// First run. The directory doesn't exist yet, so there can be
@@ -164,12 +178,12 @@ func moveDeletedFiles(fileStore storage.Storage,
 	for _, filename := range srcFiles {
 		photoId := stripExtension.ReplaceAllLiteralString(filename, "")
 
-		if !containsPhoto(photos, photoId, stderr) {
+		if !ftg.containsPhoto(photos, photoId) {
 			fmt.Printf("Moving %s of deleted photo %s to attic\n",
 				description, photoId)
 			oldPath := path.Join(srcDir, filename)
 			newPath := path.Join("attic", oldPath)
-			err = fileStore.Move(oldPath, newPath)
+			err = ftg.fileStore.Move(oldPath, newPath)
 
 			if err != nil {
 				msg := fmt.Sprintf(
@@ -183,17 +197,27 @@ func moveDeletedFiles(fileStore storage.Storage,
 	return nil
 }
 
-func containsPhoto(photos []flickrapi.PhotoListEntry,
-	idToFind string,
-	stderr io.Writer) bool {
+func (ftg *application) containsPhoto(
+	photos []flickrapi.PhotoListEntry,
+	idToFind string) bool {
 
 	for _, candidate := range photos {
 		id, err := candidate.Id()
 
 		if err != nil {
 			// Should've already been caught elsewhere. Warn and continue.
-			fmt.Fprintln(stderr, "Photo has no id")
+			fmt.Fprintln(ftg.stderr, "Photo has no id")
 		} else if id == idToFind {
+			return true
+		}
+	}
+
+	return false
+}
+
+func containsString(haystack []string, needle string) bool {
+	for _, candidate := range haystack {
+		if candidate == needle {
 			return true
 		}
 	}
